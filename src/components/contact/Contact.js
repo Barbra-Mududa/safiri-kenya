4b26b26433d3b2e7678226c213edb6a7ec276131
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-17 23:20:22 +0300

    feat: content stucture

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Contact() {
+  const handleEmailClick = () => {
+    window.location.href = 'mailto:example@example.com';
+  };
+
   return (
-    <div>Contact</div>
+    <div className='homeContainer'>
+      <form class="form">
+        <div className='contact-container'>
+          <div class="pageTitle title">
+              <img id='phone' src='https://img.freepik.com/free-vector/phone_78370-560.jpg?w=2000'/>
+              <h2>Contact Me</h2> 
+          </div>
+          <div class="secondaryTitle title">DISCUSS A PROJECT OR JUST GET IN TOUCH? MY INBOX IS OPEN</div>      
+          </div>
+          <div className='contacts'>
+            <Link to="https://github.com/Barbra-Mududa/">
+              <i class="fa-brands fa-github" id='icons'></i>
+            </Link>
+            <Link to="https://www.linkedin.com/in/barbra-mududa-30a36124b/">
+              <i class="fa-brands fa-linkedin" id='icons'></i>
+            </Link>
+            <Link onClick={handleEmailClick}>
+              <i class="fa-solid fa-envelope" id='icons'></i>
+            </Link>
+            </div>
+          <div className='contact-arrow'>
+        Go Back Home <Link to='/'>
+          <i class="fa-sharp fa-solid fa-arrow-right" id='arrow'></i>
+          </Link> 
+      </div>
+      </form> 
+    </div>
   )
 }
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import VideoLoop from './VideoLoop'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <VideoLoop />
+        <div className='hero'>
+            <h2>Adventure</h2>
+            <p>Unleash your wanderlust and let our travel tours redefine your sense of exploration.Discover captivating and awe-inspiring landscapes, and unforgettable adventures. </p>
+        </div>
+    </div>
   )
 }
 
