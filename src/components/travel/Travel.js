778a3e5bde394c56fbd6c73c23b62c7cd61f3b22
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-18 10:15:15 +0300

    feat: travels section

---
+++ src/components/travel/Travel.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Travel() {
+  return (
+    <div className='travelcont'>
+        <div className='travel'>
+
+        </div>
+        <div className='travel'>
+
+        </div>
+        <div className='travel'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Travel
