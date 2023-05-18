851cc1fd202bc150d928b9c216f4b46cef6f9ce0
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-18 10:14:12 +0300

    feat: contact component structure

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,40 +1,36 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import img from '../../assets/image 19.png'
+import './contact.css'
 
-function Contact() {
-  const handleEmailClick = () => {
-    window.location.href = 'mailto:example@example.com';
-  };
-
-  return (
-    <div className='homeContainer'>
-      <form class="form">
-        <div className='contact-container'>
-          <div class="pageTitle title">
-              <img id='phone' src='https://img.freepik.com/free-vector/phone_78370-560.jpg?w=2000'/>
-              <h2>Contact Me</h2> 
-          </div>
-          <div class="secondaryTitle title">DISCUSS A PROJECT OR JUST GET IN TOUCH? MY INBOX IS OPEN</div>      
-          </div>
-          <div className='contacts'>
-            <Link to="https://github.com/Barbra-Mududa/">
-              <i class="fa-brands fa-github" id='icons'></i>
-            </Link>
-            <Link to="https://www.linkedin.com/in/barbra-mududa-30a36124b/">
-              <i class="fa-brands fa-linkedin" id='icons'></i>
-            </Link>
-            <Link onClick={handleEmailClick}>
-              <i class="fa-solid fa-envelope" id='icons'></i>
-            </Link>
-            </div>
-          <div className='contact-arrow'>
-        Go Back Home <Link to='/'>
-          <i class="fa-sharp fa-solid fa-arrow-right" id='arrow'></i>
-          </Link> 
+const Contact = () => {
+    return (
+      <div className='cont'>
+        <div className='cont1'>
+          <img src={img} alt="real_estate_poster" />
+        </div>
+        <div className='cont2'>
+            <h1 id='heading'>Get in touch</h1>
+            <p>We'd love to hear from you.Our friendly team is always here to chat</p>
+            <form action="#" id='contact_form'>
+              <label htmlFor='name' className='label1'>Name</label> <br/>
+              <input type="text" id='name' name='Name' placeholder='Enter your name'/><br/>
+              <label htmlFor='email' className='label1'>Email</label> <br/>
+              <input type="text" id='email' name='Email' placeholder='Enter your email'/><br/>
+              <label htmlFor='number' className='label1'>Phone number</label> <br/>
+              <input type="number" id='number' name='Number' placeholder='Enter your number'/><br/>
+              <label htmlFor='text' className='label1'>How can we help?</label> <br/>
+              <textarea type="text-area" id='text' name='Text' placeholder='' rows='5' cols='57'/><br />
+              <h4 id='hd4'>Service required</h4> 
+              <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
+              <label htmlFor="vehicle1" className='label2'> Camping Sites </label><br/>
+              <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
+              <label htmlFor="vehicle2" className='label2'> Hiking Trails</label><br/>
+              <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat"/>
+              <label htmlFor="vehicle3" className='label2'>Wild Safaris</label><br/> <br />
+              <input type="submit" value="Get Started" id='submit'/>
+            </form>
+        </div>
       </div>
-      </form> 
-    </div>
-  )
-}
-
-export default Contact
+    )}
+  
+export default Contact;
