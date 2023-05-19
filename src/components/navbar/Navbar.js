8737bdd51f289a5940b631432ea97d22c1ea0778
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-20 01:34:09 +0300

    feat: include navbar coponent

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
+
+
+function Navbar({ user, setUser }) {
+  const aLink = () => {
+    const links = document.querySelector(".middle-nav");
+    const menu = document.querySelector(".menu");
+    links.classList.remove("active");
+    menu.classList.remove("clicked");
+  };
+
+  return (
+    <nav>
+      <div className="nav-header">
+        <NavLink to="/" id="header" onClick={aLink}>
+          Safiri Kenya
+        </NavLink>
+      </div>
+
+      <div className="middle-nav">
+        <div className="container1-nav">
+          <NavLink to="/about" className="navlink" onClick={aLink}>
+            About
+          </NavLink>
+          <NavLink to="/travel" className="navlink" onClick={aLink}>
+            Experience
+          </NavLink>
+          <NavLink to="/blog" className="navlink" onClick={aLink}>
+            Blog
+          </NavLink>
+          <NavLink to="/contact" className="navlink" onClick={aLink}>
+            Contact{" "}
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
