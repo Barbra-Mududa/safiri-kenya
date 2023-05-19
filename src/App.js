f979d4ea480180d526c4fe8ba4d2e5448607fd5f
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-20 01:35:10 +0300

    feat: establish routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Home from './components/home/Home';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/Home';
+import Travel from './components/travel/Travel';
+import Navbar from './components/navbar/Navbar';
+import Contact from './components/contact/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className='app'>
+      <Router> 
+        <Navbar />
+        <Routes>
+        <Route path="/about" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/travel" element={<Travel />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router> 
     </div>
   );
 }
