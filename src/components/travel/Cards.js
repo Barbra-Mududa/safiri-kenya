fa7beda9cdddfca4df0954e5c9d73354481c622f
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-19 05:27:26 +0300

    feat: card structure, filter and mapping

--- src/components/travel/Cards.js
+++ src/components/travel/Cards.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import './travel.css'
 
-function Cards() {
-  return (
-    <div>Cards</div>
-  )
+
+function Card({card}) {
+
+
+    return (
+        <div className='card-container' >
+            <figure>
+            <div className="card shadow s-white rounded text-white">
+                <img className="card-img" src={card.image} alt="Card image"/>
+                <figcaption>
+                <div class="card-img-overlay d-flex flex-column my-text">
+                        <h2 id='overlay-text'><strong>{card.title}</strong></h2>
+                        <p id='overlay-text' className='htxt'>{card.location}</p>
+                        <p id='overlay-text'>{card.description}</p>
+                        {/* <Link to={`/singleproperty/${card.id}`} >
+                            <span class= 'v-btn'><button></button></span>                        
+                        </Link> */}
+                </div>
+                </figcaption>
+            </div>
+            </figure>
+        </div>
+    )
 }
 
-export default Cards
+export default Card
