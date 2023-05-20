d82a8759e3fe7ee68d1768dc12d918242926818b
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-21 00:16:49 +0300

    feat: adding about section feature

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import img from '/home/barbra/Documents/safiri-kenya/src/assets/image 20.png';
+import { TypeAnimation } from 'react-type-animation';
+import "../home/home.css"
 
 function About() {
+
   return (
-    <div>About</div>
+    <div>
+      <img id='homeimg' src={img} alt='' />
+      <div className='hero'>
+            <h1 id='type-animation'>
+            <TypeAnimation
+            sequence={[
+                'SAFIRI KENYA', 
+                3000, // Waits 3s
+                'KENYA ADVENTURES', 
+                3000, // Waits 3s
+                () => {
+                console.log('Done typing!'); 
+                }
+            ]}
+            wrapper="div"
+            cursor={true}
+            repeat={Infinity}
+            style={{ fontSize: '2.5em' }}
+            />
+            </h1>            <p>Unleash your wanderlust and let our travel tours redefine your sense of exploration.Discover captivating and awe-inspiring landscapes, and unforgettable adventures. </p>
+        </div>
+        <div className='experience'>
+            <h3>EXPERIENCE</h3>
+            <h2>Thrilling Adventures</h2>
+            <p>Embark on unforgettable adventures through exhilarating  trails, immersive camping experiences, and breathtaking  
+                Kenya Safaris that will ignite your spirit.</p>
+        </div>
+    </div>
   )
 }
 
