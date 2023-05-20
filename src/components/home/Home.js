4a7e1fd54ad298af4dbea9b5e7ab2af0aab02fc2
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-21 00:20:09 +0300

    refactor: home section re-structure

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { TypeAnimation } from 'react-type-animation';
-import img from '/home/barbra/Documents/safiri-kenya/src/assets/image 20.png';
 import img1 from '../../El Karama.png'
 import img2 from '../../HGate.png'
 import img3 from '../../Mt. Kenya.png'
@@ -8,38 +6,13 @@ import './home.css';
 import Travel from '../travel/Travel';
 import Footer from '../footer/Footer';
 import Navbar from '../navbar/Navbar';
-
+import About from '../about/About';
 
 function Home() {
   return (
 
     <div className='home'>
-        <img id='homeimg' src={img} alt='' />
-        <div className='hero'>
-            <h1 id='type-animation'>
-            <TypeAnimation
-            sequence={[
-                'SAFIRI KENYA', 
-                3000, // Waits 3s
-                'KENYA ADVENTURES', 
-                3000, // Waits 3s
-                () => {
-                console.log('Done typing!'); 
-                }
-            ]}
-            wrapper="div"
-            cursor={true}
-            repeat={Infinity}
-            style={{ fontSize: '2.5em' }}
-            />
-            </h1>            <p>Unleash your wanderlust and let our travel tours redefine your sense of exploration.Discover captivating and awe-inspiring landscapes, and unforgettable adventures. </p>
-        </div>
-        <div className='experience'>
-            <h3>EXPERIENCE</h3>
-            <h2>Thrilling Adventures</h2>
-            <p>Embark on unforgettable adventures through exhilarating  trails, immersive camping experiences, and breathtaking  
-                Kenya Safaris that will ignite your spirit.</p>
-        </div>
+        <About />
         <div className='homecards'>
             <div id='img1'>
                 <img src={img1}/>
